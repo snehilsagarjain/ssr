2353c91a6228fa644d118ecae895be679983d25b
Snehil sagar jain <42835593+snehilsagarjain@users.noreply.github.com>
2025-01-08 19:51:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
+require("@babel/register")
+
+require('./app')
+// const express = require('express');
+// const app = express();
+
+// const PORT = 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
